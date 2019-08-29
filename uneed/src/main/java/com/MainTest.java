57da78d65c46fe3d64a823360ba71fea2cf01863
WangYekun <849907942@qq.com>
2019-08-29 14:34:22 +0800

    add uneed project for replaceStr

---
+++ uneed/src/main/java/com/MainTest.java
@@ -0,0 +1,58 @@
+package com;
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author WangYeKun
+ * @version 1.0
+ * @date 2019/8/29 11:16
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("============直接替换系统属性值===============");
+        System.out.println(StrSubstitutor.replaceSystemProperties("You are running with java.version = " +
+                "${java.version} and os.name = ${os.name}."));
+
+        Map valuesMap = new HashMap();
+        valuesMap.put("animal", "quick brown fox");
+        valuesMap.put("target", "lazy dog");
+        System.out.println("============使用Map替换字符串中的占位符===============");
+        String templateString = "The ${animal} jumped over the ${target}.";
+        StrSubstitutor sub = new StrSubstitutor(valuesMap);
+        String resolvedString = sub.replace(templateString);
+        System.out.println("resolvedString = " + resolvedString);
+
+        StrSubstitutor.replaceSystemProperties("You are running with java.version = ${java.version} " +
+                "and os.name = ${os.name}.");
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "${x}");
+        params.put("x", "replaceSystemAttributeValues");
+        StrSubstitutor strSubstitutor = new StrSubstitutor(params);
+        String hello2 = "${name}";
+        System.out.println("============直接替换系统属性值===================");
+        System.out.println(strSubstitutor.replace(hello2));
+
+        System.out.println("============设置默认值===================");
+        Map values = new HashMap();
+        values.put("animal", "quick brown fox");
+        values.put("target", "");
+        String template = "The ${animal} jumped over the ${target}${undefined.number:-1234567890}";
+        StrSubstitutor s = new StrSubstitutor(values);
+        String sl = s.replace(template);
+        System.out.println("sl = " + sl);
+
+        System.out.println("============简单的调用-自定义匹配符===================");
+        Map valuesMaps = new HashMap();
+        valuesMaps.put("animal", "quick brown fox");
+        valuesMaps.put("target", "lazy dog");
+        String templateStrings = "The &(animal) jumped over the &(target). &(undefined.number:-1234567890).";
+        StrSubstitutor subs = new StrSubstitutor(valuesMaps, "&(", ")", '&');
+        String resolvedStrings = subs.replace(templateStrings);
+        System.out.println("resolvedStrings = " + resolvedStrings);
+    }
+
+
+}
