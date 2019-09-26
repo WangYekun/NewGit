3db5556d26bf5715e8ac5c7da5a043ef80112f08
WangYekun <849907942@qq.com>
2019-09-26 18:32:23 +0800

    add file

--- uneed/src/main/java/com/ObjectsEquals.java
+++ uneed/src/main/java/com/ObjectsEquals.java
@@ -11,32 +11,17 @@ public class ObjectsEquals {
     public static void main(String[] args) {
         // 避免被零除
         Double total = 100D;
-        Double cost = 0D;
-        Double avg = 0D;
-        if (!Objects.equals(cost, 0)) {
-            avg = total / cost;
+        Double cost = 0.0E00;
+        if (Objects.equals(cost, 0)) {
         }
 
-        // 切记，比较的需要时相同的类
-        if (!Objects.equals(cost, 0D)) {
-            avg = total / cost;
-        }
-
-        if (cost != 0) {
-            avg = total / cost;
-        }
         Double a = 0D;
         Double d = 0D;
         int f = 0;
-        boolean result = Objects.equals(a, d);
         boolean result1 = Objects.equals(a, f);
         // true
-        System.out.println(result);
+        System.out.println(true);
         // false
         System.out.println(result1);
-        // true
-        System.out.println(a == f);
-
-        System.out.println(a.equals(d));
     }
 }
