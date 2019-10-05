a1236f9be159c11b2ef292cb0758bbb7a4607c54
WangYekun <849907942@qq.com>
2019-10-05 13:01:08 +0800

    file

--- uneed/src/main/java/uneed/Sql.java
+++ uneed/src/main/java/uneed/Sql.java
@@ -13,5 +13,13 @@ public class Sql {
         // 时间格式:年-月-日 时:分:秒
         new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println("#######@@@@@@@@@@@###########");
+        /*
+        pk_ 即primary key；uk_ 即 unique key；idx_ 即index的简称
+        对象 年龄区间 类型 字节 表示范围
+        人 150岁之内 tinyint unsigned 1 无符号值：0到255
+        龟 数百岁 smallint unsigned 2 无符号值：0到65535
+        恐龙化石 数千万年 int unsigned 4 无符号值：0到约42.9亿
+        太阳 约50亿年 bigint unsigned 8 无符号值：0到约10的19次方
+        * */
     }
 }
