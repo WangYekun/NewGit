85660e1691602ef1130e2a28634b08c7fa51ea83
WangYekun <849907942@qq.com>
2019-10-12 12:03:12 +0800

    file

--- SpringBootValidateCopy/src/main/java/top/billie/controller/UsersController.java
+++ SpringBootValidateCopy/src/main/java/top/billie/controller/UsersController.java
@@ -1,5 +1,7 @@
 package top.billie.controller;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,38 +14,40 @@ import javax.validation.Valid;
 /**
  * @program: SpringBootValidate
  * @description:
- * @author: Billie
+ * @author: WangYeKun
  * @create: 2019-06-27 17:07
  **/
 @Controller
+@Slf4j
 public class UsersController {
 
     /**
-    * @Description: 添加数据的控制器
-    * @Param: [users]
-    * @return: java.lang.String
-    * @Author: Billie
-    * @Date: 2019/6/27
-    */
+     * @Description: 添加数据的控制器
+     * @Param: [users]
+     * @return: java.lang.String
+     * @Author: Billie
+     * @Date: 2019/6/27
+     */
     @RequestMapping("/sign_up")
     public String showPage(@ModelAttribute("register") Users users) {
         return "register";
     }
 
     /**
-    * @Description: 保存数据的控制器
-     *
-    * @Param: [users, result]
-    * @return: java.lang.String
-    * @Author: Billie
-    * @Date: 2019/6/27
-    */
+     * @Description: 保存数据的控制器
+     * @Param: [users, result]
+     * @return: java.lang.String
+     * @Author: Billie
+     * @Date: 2019/6/27
+     */
     @RequestMapping("/save")
     public String saveUser(@ModelAttribute("register") @Valid Users users, BindingResult result) {
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             return "register";
         }
-        System.out.println(users);
+
+        log.info("======== register users information ==========: {}", JSON.toJSONString(users));
+
         return "success";
     }
 
--- SpringBootValidateCopy/src/main/java/top/billie/pojo/Users.java
+++ SpringBootValidateCopy/src/main/java/top/billie/pojo/Users.java
@@ -4,17 +4,24 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Min;
 
 /**
  * @program: SpringBootValidate
  * @description:
- * @author: Billie
+ * @author: WangYeKun
  * @create: 2019-06-27 17:11
  **/
 @Data
 public class Users {
+
+
+    @Future()
+    @Range(min = 10, max = 20, message = "11<X<21")
+    private String registerDate;
 
     @NotBlank(message = "用户名不能为空")
     @Length(min = 8, max = 16, message = "请输入8-16位用户名")
@@ -31,8 +38,4 @@ public class Users {
     @NotBlank(message = "密码不能为空")
     private String email;
 
-    @Override
-    public String toString() {
-        return "Users{" + "name='" + name + '\'' + ", password='" + password + '\'' + ", age=" + age + ", email='" + email + '\'' + '}';
-    }
 }
