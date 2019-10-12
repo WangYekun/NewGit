2fa23751bc1f2fb06a08ad0641dd083fe246e64f
WangYekun <849907942@qq.com>
2019-10-12 11:02:24 +0800

    validate

--- SpringBootValidateCopy/src/main/java/top/billie/pojo/Users.java
+++ SpringBootValidateCopy/src/main/java/top/billie/pojo/Users.java
@@ -1,5 +1,6 @@
 package top.billie.pojo;
 
+import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
@@ -12,56 +13,26 @@ import javax.validation.constraints.Min;
  * @author: Billie
  * @create: 2019-06-27 17:11
  **/
+@Data
 public class Users {
+
     @NotBlank(message = "用户名不能为空")
-    @Length(min = 8,max = 16,message = "请输入8-16位用户名")
+    @Length(min = 8, max = 16, message = "请输入8-16位用户名")
     private String name;
+
     @NotBlank(message = "密码不能为空")
+    @Length(min = 8, max = 16, message = "请输入8-16位密码")
     private String password;
-    @Min(value = 18,message = "本网站仅限成年人注册")
+
+    @Min(value = 18, message = "本网站仅限成年人注册")
     private Integer age;
+
     @Email
+    @NotBlank(message = "密码不能为空")
     private String email;
 
     @Override
     public String toString() {
-        return "Users{" +
-                "name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        return "Users{" + "name='" + name + '\'' + ", password='" + password + '\'' + ", age=" + age + ", email='" + email + '\'' + '}';
     }
 }
