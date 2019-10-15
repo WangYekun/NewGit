328f5c592c8aaa0fa846dee26cec2a8ae57ff342
WangYekun <849907942@qq.com>
2019-10-15 09:53:35 +0800

    add file“

---
+++ uneed/src/main/java/com/MyBatisForeach.java
@@ -0,0 +1,89 @@
+package com;
+
+/**
+ * @author WangYeKun
+ * @version 1.0
+ * @date 2019/10/15 9:44
+ */
+public class MyBatisForeach {
+
+    /**
+     * 1.单参数List的类型：
+     *   <select id="dynamicForeachTest" resultType="Blog">
+     *           select * from t_blog where id in
+     *        <foreach collection="list" index="index" item="item" open="(" separator="," close=")">
+     *                #{item}       
+     *        </foreach>    
+     *    </select>
+     *
+     * 上述collection的值为list，对应的Mapper是这样的
+     * public List dynamicForeachTest(List ids);
+     * 测试代码：
+     *       @Test
+     *     public void dynamicForeachTest() {
+     *         SqlSession session = Util.getSqlSessionFactory().openSession();      
+     *         BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+     *          List ids = new ArrayList();
+     *          ids.add(1);
+     *          ids.add(3);
+     *           ids.add(6);
+     *         List blogs = blogMapper.dynamicForeachTest(ids);
+     *          for (Blog blog : blogs)
+     *              System.out.println(blog);
+     *          session.close();
+     *      }
+     *
+     * 2.单参数array数组的类型：
+     * <select id="dynamicForeach2Test" resultType="Blog">
+     *           select * from t_blog where id in
+     *        <foreach collection="array" index="index" item="item" open="(" separator="," close=")">                               #{item}
+     *          </foreach>
+     *      </select>
+     *
+     * 上述collection为array，对应的Mapper代码：
+     * public List dynamicForeach2Test(int[] ids);
+     * 对应的测试代码：
+     *       @Test
+     *     public void dynamicForeach2Test() {
+     *         SqlSession session = Util.getSqlSessionFactory().openSession();
+     *         BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+     *         int[] ids = new int[] {1,3,6,9};
+     *         List blogs = blogMapper.dynamicForeach2Test(ids);
+     *         for (Blog blog : blogs)
+     *         System.out.println(blog);    
+     *         session.close();
+     *       }
+     *
+     * 3.自己把参数封装成Map的类型
+     * <select id="dynamicForeach3Test" resultType="Blog">
+     *         select * from t_blog where title like "%"#{title}"%" and id in
+     *          <foreach collection="ids" index="index" item="item" open="(" separator="," close=")">
+     *               #{item}
+     *          </foreach>
+     *     </select>
+     *
+     * 上述collection的值为ids，是传入的参数Map的key，对应的Mapper代码：
+     * public List dynamicForeach3Test(Map params);
+     * 对应测试代码：
+     *       @Test
+     *     public void dynamicForeach3Test() {
+     *         SqlSession session = Util.getSqlSessionFactory().openSession();
+     *          BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+     *           final List ids = new ArrayList();
+     *           ids.add(1);
+     *           ids.add(2);
+     *           ids.add(3);
+     *           ids.add(6);
+     *          ids.add(7);
+     *          ids.add(9);
+     *         Map params = new HashMap();
+     *          params.put("ids", ids);
+     *          params.put("title", "中国");
+     *         List blogs = blogMapper.dynamicForeach3Test(params);
+     *          for (Blog blog : blogs)
+     *              System.out.println(blog);
+     *          session.close();
+     *      }
+     */
+
+}
--- uneed/src/main/resources/EnhanceFor.md
+++ uneed/src/main/resources/EnhanceFor.md
@@ -1,4 +1,5 @@
 # 增强for用法
+
 > 增强for循环(掌握)
  
 > (1)是for循环的一种
@@ -9,5 +10,25 @@
  
 > (3)好处：简化了数组和集合的遍历
 
->(4)弊端
+> (4)弊端
  增强for循环的目标不能为null。建议在使用前，先判断是否为null。
+
+# MyBatis中的foreach循环
+
+> foreach元素的属性主要有 item，index，collection，open，separator，close。
+> item表示集合中每一个元素进行迭代时的别名，index指 定一个名字，用于表示在迭代过程中，每次迭代到的位置，
+> open表示该语句以什么开始，separator表示在每次进行迭代之间以什么符号作为分隔符，
+> close表示以什么结束，在使用foreach的时候最关键的也是最容易出错的就是collection属性，该属性是必须指定的
+
+
+- 如果传入的是单参数且参数类型是一个List的时候，collection属性值为list  
+- 如果传入的是单参数且参数类型是一个array数组的时候，collection的属性值为array
+- 如果传入的参数是多个的时候
+
+> 我们就需要把它们封装成一个Map了，当然单参数也可以封装成map，
+> 实际上如果你在传入参数的时候，在breast里面也是会把它封装成一个Map的，
+> map的key就是参数名，所以这个时候collection属性值就是传入的List或array对象在自己封装的map里面的key
+
+
+
+  
