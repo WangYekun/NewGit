5e6a0d0ca12512c0f1fd963c8616ee2e1648031f
WangYekun <849907942@qq.com>
2019-10-18 00:10:16 +0800

    add file

--- first-ek-service-provider/src/main/java/org/crazyit/cloud/FirstServiceProvider.java
+++ first-ek-service-provider/src/main/java/org/crazyit/cloud/FirstServiceProvider.java
@@ -1,3 +1,4 @@
+/*
 package org.crazyit.cloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,4 +12,28 @@ public class FirstServiceProvider {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(FirstServiceProvider.class).run(args);
 	}
+}
+*/
+
+
+package org.crazyit.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FirstServiceProvider {
+
+    public static void main(String[] args) {
+        // 读取控制台输入的端口，避免端口冲突
+        System.out.println("===================请输入两个端口=====================");
+        Scanner scan = new Scanner(System.in);
+        String port = scan.nextLine();
+        new SpringApplicationBuilder(FirstServiceProvider.class).properties("server.port=" + port).run(args);
+
+    }
 }
--- first-ek-service-provider/src/main/java/org/crazyit/cloud/Person.java
+++ first-ek-service-provider/src/main/java/org/crazyit/cloud/Person.java
@@ -1,3 +1,4 @@
+/*
 package org.crazyit.cloud;
 
 public class Person {
@@ -41,6 +42,65 @@ public class Person {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+}
+*/
+
+
+package org.crazyit.cloud;
+
+public class Person {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String message;
+
+    public Person() {
+        super();
+    }
+
+    public Person(Integer id, String name, Integer age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
 }
