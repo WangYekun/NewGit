d9c5efcbc2b0ed38c283fb02c7ceb9cfcdac703c
WangYekun <849907942@qq.com>
2019-10-21 21:34:20 +0800

    add map 遍历学习

--- uneed/src/main/java/uneed/MapContainsKey.java
+++ uneed/src/main/java/uneed/MapContainsKey.java
@@ -1,7 +1,9 @@
 package uneed;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author WangYeKun
@@ -10,6 +12,7 @@ import java.util.Map;
  */
 public class MapContainsKey {
     public static void main(String[] args) {
+        System.out.println("###################### map学习中的containKey()方法 ##################");
 
         // 定义Map对象
         Map<String, String> map = new HashMap<>(16);
@@ -36,6 +39,28 @@ public class MapContainsKey {
             System.out.println("在Map集合中不包含键名===>:" + key);
             System.out.println("在Map集合中不包含键名===>:" + key2);
             System.out.println("在Map集合中不包含键名===>:" + key3);
+        }
+
+        System.out.println("###################### map学习 ##################");
+
+        // 1. 定义集合
+        Map<String, String> mapStr = new HashMap<>(16);
+
+        // 2. 往集合里面加元素
+        mapStr.put("key", "value");
+
+        // 3. 遍历集合(以增强for遍历)
+        Set<String> keys = mapStr.keySet();
+        for (String keyCopy : keys) {
+            System.out.println("key=========>" + keyCopy + "||value============>" + mapStr.get(keyCopy));
+        }
+
+        // 4. 遍历集合(以迭代器遍历)
+        Set<String> keyIt = mapStr.keySet();
+        Iterator<String> keyIt2 = keyIt.iterator();
+        while (keyIt2.hasNext()) {
+            String keyLoop = keyIt2.next();
+            System.out.println("key=========>" + keyLoop + "||value============>" + mapStr.get(keyLoop));
         }
     }
 }
