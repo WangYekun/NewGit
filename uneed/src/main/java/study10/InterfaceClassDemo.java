bf0f1ef041d7f48cd3a2d5e230000312f1750b34
WangYekun <849907942@qq.com>
2019-10-25 11:35:55 +0800

    add mysql_file

---
+++ uneed/src/main/java/study10/InterfaceClassDemo.java
@@ -0,0 +1,14 @@
+package study10;
+
+/**
+ * @author WangYeKun
+ * @version 1.0
+ * @date 2019/10/22 22:19
+ * @description 面向对象练习
+ */
+public class InterfaceClassDemo {
+    public static void main(String[] args) {
+        System.out.println("############################## interface class ###################################");
+        
+    }
+}
