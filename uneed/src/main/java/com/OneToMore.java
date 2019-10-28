3081a6f19d74657659bb618d16706b811408e5da
WangYekun <849907942@qq.com>
2019-10-28 16:08:13 +0800

    update file

--- uneed/src/main/java/com/OneToMore.java
+++ uneed/src/main/java/com/OneToMore.java
@@ -21,11 +21,14 @@ public class OneToMore {
         list.add("b");
         list.add("c");
         map.put(1, list);
+
         // 得到key
         List<String> results = map.get(1);
+
         // 输出key(List集合方式存在)
         System.out.println("######################## 集合方式存在 ##############################");
         System.out.println("key = " + results);
+
         // 增强for输出
         System.out.println("######################## 循环方式存在 ##############################");
         for (Object key : results) {
