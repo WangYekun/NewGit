99f7102621ee6dfad70c4732a694156b2af1bb08
WangYekun <849907942@qq.com>
2019-11-23 12:48:11 +0800

    add swagger

--- uneed/src/main/java/study11/mapfour/MapFourTodo.java
+++ uneed/src/main/java/study11/mapfour/MapFourTodo.java
@@ -18,40 +18,41 @@ public class MapFourTodo {
         Map<String, Student> stu = Maps.newHashMap();
 
         //添加元素
+        System.out.println("-----------//添加元素--------------");
         stu.put("0001", new Student("sunming1"));
         stu.put("0002", new Student("sunming2"));
         stu.put("0003", new Student("sunmnig3"));
         System.out.println(stu);
-        System.out.println("-------------------------");
 
         //删除元素
+        System.out.println("------------//删除元素-------------");
         stu.remove("0001");
         System.out.println(stu);
-        System.out.println("-------------------------");
 
         //更新元素
+        System.out.println("------------//更新元素-------------");
         stu.put("0002", new Student("sunmingUpdate"));
         System.out.println(stu);
-        System.out.println("-------------------------");
 
         //查询元素
+        System.out.println("----------- //查询元素--------------");
         System.out.println(stu.get("0003"));
-        System.out.println("-------------------------");
 
         //获取所有的key和value
+        System.out.println("----------//获取所有的key和value---------------");
         for (Map.Entry<String, Student> entry : stu.entrySet()) {
             System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
         }
-        System.out.println("-------------------------");
 
         //获取所有的key
+        System.out.println("-----------//获取所有的key--------------");
         Set<String> keys = stu.keySet();
         for (String key : keys) {
             System.out.println(key);
         }
-        System.out.println("-------------------------");
 
-        //获取所有的value
+
+        System.out.println("------------------//获取所有的value---------------");
         Collection<Student> values = stu.values();
         for (Student value : values) {
             System.out.println(value);
--- uneed/src/main/java/study11/mapfour/Student.java
+++ uneed/src/main/java/study11/mapfour/Student.java
@@ -1,11 +1,14 @@
 package study11.mapfour;
 
+import lombok.Data;
+
 /**
  * @author WangYeKun
  * @version 1.0
  * @date 2019/11/14 18:01
  * @description 学生类
  */
+@Data
 public class Student {
 
     private String id;
@@ -14,22 +17,6 @@ public class Student {
     public Student(String strName) {
         name = strName;
         id = "00";
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
