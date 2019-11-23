a03a277b048e78be66d3fc25e62b77aa3fd75e2b
WangYekun <849907942@qq.com>
2019-11-23 12:55:24 +0800

    add swagger

--- springboot-swagger/src/main/java/com/itguang/springbootswaggerdemo1/SpringbootSwaggerDemo1Application.java
+++ springboot-swagger/src/main/java/com/itguang/springbootswaggerdemo1/SpringbootSwaggerDemo1Application.java
@@ -3,6 +3,9 @@ package com.itguang.springbootswaggerdemo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lenovo
+ */
 @SpringBootApplication
 public class SpringbootSwaggerDemo1Application {
 
