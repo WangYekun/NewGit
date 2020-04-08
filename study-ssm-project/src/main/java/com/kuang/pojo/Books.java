77b9799af7044c63a82a831ef3f87a3278876174
WangYekun <849907942@qq.com>
2020-04-09 00:06:37 +0800

    fix: add project by ssm

---
+++ study-ssm-project/src/main/java/com/kuang/pojo/Books.java
@@ -0,0 +1,23 @@
+package com.kuang.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Mark
+ * @version 1.0
+ * @date 2020/4/8 22:09
+ * @description 书籍实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Books {
+
+    private int bookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+
+}
