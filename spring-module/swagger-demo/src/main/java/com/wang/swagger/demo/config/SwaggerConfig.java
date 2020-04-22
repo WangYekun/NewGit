7464a89fbbf4051a75923b40a93483bbeaf381bb
WangYekun <849907942@qq.com>
2020-04-22 14:21:19 +0800

    docx: add environment by swagger

--- spring-module/swagger-demo/src/main/java/com/wang/swagger/demo/config/SwaggerConfig.java
+++ spring-module/swagger-demo/src/main/java/com/wang/swagger/demo/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -25,9 +27,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket docket() {
+    public Docket docket(Environment environment) {
+
+        Profiles of = Profiles.of("dev");
+
+        boolean b = environment.acceptsProfiles(of);
+        System.out.println("b = " + b);
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("WangYeKun")
+                // enable to environment config
+                .enable(b)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.wang.swagger.demo.rest"))
