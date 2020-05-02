9314cce64de1d80c2de19d8f0b5441a450234a7e
WangYekun <849907942@qq.com>
2020-05-02 19:40:10 +0800

    docx: format code

--- spring-module/redis-demo/src/main/java/wang/demo/redis/redis/RedisApplication.java
+++ spring-module/redis-demo/src/main/java/wang/demo/redis/redis/RedisApplication.java
@@ -3,11 +3,14 @@ package wang.demo.redis.redis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lenovo
+ */
 @SpringBootApplication
 public class RedisApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
-    }
+        System.out.println("################# RedisApplication is started ##########################");
 
+    }
 }
