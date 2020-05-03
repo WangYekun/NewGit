ac3558c09116959062d0bab2cc57df3e9d812d9d
WangYekun <849907942@qq.com>
2020-05-03 17:09:30 +0800

    feat: add practice OSS

---
+++ spring-module/redis-demo/src/main/java/wang/demo/redis/redis/service/OssService.java
@@ -0,0 +1,15 @@
+package wang.demo.redis.redis.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mark
+ * @version 1.0
+ * @date 2020/5/3 11:54
+ * @description OSS文件上传下载服务
+ */
+@Service
+public class OssService {
+
+
+}
