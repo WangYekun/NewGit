554af2c4a186d0a2f567da6f393a1990eb6acd73
WangYekun <849907942@qq.com>
2020-05-04 13:41:47 +0800

    feat: add jedis operation

---
+++ spring-module/redis-demo/src/main/java/wang/demo/redis/redis/jedis/JedisTest.java
@@ -0,0 +1,10 @@
+package wang.demo.redis.redis.jedis;
+
+/**
+ * @author Mark
+ * @version 1.0
+ * @date 2020/5/4 12:21
+ * @description Jedis学习
+ */
+public class JedisTest {
+}
---
+++ spring-module/redis-demo/src/main/java/wang/demo/redis/redis/jedis/JedisTestTx.java
@@ -0,0 +1,10 @@
+package wang.demo.redis.redis.jedis;
+
+/**
+ * @author Mark
+ * @version 1.0
+ * @date 2020/5/4 12:27
+ * @description jedis事务测试
+ */
+public class JedisTestTx {
+}
