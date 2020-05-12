968d097f6c5e8e8f48f68c9bda87869f2b0bf42c
WangYekun <849907942@qq.com>
2020-05-12 19:38:41 +0800

    docx: format code

--- spring-module/spring-04-crud/src/main/java/study/dao/UserMapper.java
+++ spring-module/spring-04-crud/src/main/java/study/dao/UserMapper.java
@@ -10,15 +10,53 @@ import study.pojo.User;
  */
 
 public interface UserMapper {
+
+    /**
+     * 删除
+     *
+     * @param id 主键id
+     * @return 删除标志(int)
+     */
     int deleteByPrimaryKey(Integer id);
 
+    /**
+     * 新增
+     *
+     * @param record 学生对象
+     * @return 参数
+     */
     int insert(User record);
 
+    /**
+     * 新增(属性为空也行)
+     *
+     * @param record 学生对象
+     * @return int
+     */
     int insertSelective(User record);
 
+    /**
+     * 查询
+     *
+     * @param id 主键id
+     * @return User用户对象
+     */
     User selectByPrimaryKey(Integer id);
 
+    /**
+     * 更新
+     *
+     * @param record 学生对象
+     * @return 更新(1为成功, 0为失败)
+     */
     int updateByPrimaryKeySelective(User record);
 
+    /**
+     * 更新
+     *
+     * @param record 学生对象
+     * @return 更新(1为成功, 0为失败)
+     */
     int updateByPrimaryKey(User record);
+
 }
